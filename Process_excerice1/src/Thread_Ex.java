090543e47ce967f4e845a716d72b4a03f635e11a
ericandreopoulos <ericandreopoulos@adminisatorsmbp.jestais.local>
2019-09-11 15:17:44 -0400

    initial

---
+++ Process_excerice1/src/Thread_Ex.java
@@ -0,0 +1,22 @@
+
+public class Thread_Ex extends Thread{
+	
+	String name;
+	
+	public Thread_Ex(String name) {
+		this.name = name;
+	}
+	
+	public void run(String name) {
+		System.out.println("Hi my name is: " + name);
+	}
+
+	public static void main(String[] args) {
+		Thread_Ex t1 = new Thread_Ex("alpha");
+		Thread_Ex t2 = new Thread_Ex("beta");
+		
+		t1.start();
+		t2.start();
+	}
+
+}
